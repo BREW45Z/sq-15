788c2734c132033bedab463b15370da0f24b075f
apple <bstar@PANDORAS-BOX.local>
2023-02-11 20:07:15 +0100

    added new feature

---
+++ day 1/index.js
@@ -0,0 +1,6 @@
+ let arrNum  = [4,5,3,1,8,0,9,2,7,6];
+ let target = 7;
+
+ let sortedArr = [0,1,2,3,4,5,6,7,8,9,10];
+let sorted = arr.Num.sort((a, b) => b-a);
+console.log("sorted array", sorted);
